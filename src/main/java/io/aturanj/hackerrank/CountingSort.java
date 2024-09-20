f46f7e970caa6c79e45ffe8d631d738208db3482
aturanj <aturan@tuta.io>
2024-09-20 14:49:41 +0100

    feat: add CountingSort solution

---
+++ src/main/java/io/aturanj/hackerrank/CountingSort.java
@@ -0,0 +1,65 @@
+package io.aturanj.hackerrank;
+
+import io.aturanj.hackerrank.LonelyInteger.Result;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+
+/**
+ * https://www.hackerrank.com/challenges/one-week-preparation-kit-countingsort1
+ */
+class CountingSortResult {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+
+        ArrayList list = new ArrayList<Integer>();
+
+        for (int i = 0; i < 100; i++) {
+            list.add(Collections.frequency(arr, i));
+        }
+
+        return list;
+    }
+
+}
+
+public class CountingSort {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = CountingSortResult.countingSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
